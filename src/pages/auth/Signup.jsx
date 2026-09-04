3dcd2e6340adf2e31ca1e8a449ec1fca289eeca8
Ridwane <emileadeboye2@gmail.com>
2026-09-04 10:34:38 +0100

    correction

--- src/pages/auth/Signup.jsx
+++ src/pages/auth/Signup.jsx
@@ -93,7 +93,7 @@ export default function Signup() {
           <div className="relative">
             <User className="w-4 h-4 text-slate-400 absolute left-3 top-1/2 -translate-y-1/2" />
             <input
-              type="email"
+              type="name"
               value={nom}
               onChange={(e) => setNom(e.target.value)}
               placeholder="Mamadou Diallo"
